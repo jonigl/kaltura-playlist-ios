8faa61fd2afcee8681f7ee86add805674de7d0ba
Jonathan <jonigl@gmail.com>
2016-03-21 18:08:52 -0300

    Trying to get playerPlaybackStateDidChange work

--- kaltura-playlist-ios/ViewController.m
+++ kaltura-playlist-ios/ViewController.m
@@ -7,12 +7,15 @@
 //
 
 #import "ViewController.h"
+#import <KALTURAPlayerSDK/KPViewController.h>
 
-@interface ViewController ()
-
+@interface ViewController () <KPViewControllerDelegate>
+@property (retain, nonatomic) KPViewController *player;
 @end
 
-@implementation ViewController
+@implementation ViewController {
+    KPPlayerConfig *config;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -22,6 +25,49 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)kPlayer:(KPViewController *)_player playerPlaybackStateDidChange:(KPMediaPlaybackState)state{
+    NSLog(@"player state %ld", (long)state);
+    if (state == KPMediaPlaybackStateEnded){
+        NSLog(@"entry played");
+    }
+}
+
+- (KPViewController *)player {
+    if (!_player) {
+        // Account Params
+        config = [[KPPlayerConfig alloc] initWithServer:@"http://vodgc.com"
+                                               uiConfID:@"23448994"
+                                              partnerId:@"109"];
+        // Video Entry
+        config.entryId = @"0_0yazfkud";
+        //        [config setEntryId:@"0_79j3ff7e"];
+        
+        // Setting this property will cache the html pages in the limit size
+        //        config.cacheSize = 0.8;
+        
+        [config addConfigKey:@"autoPlay" withValue:@"true"];
+        [self hideHTMLControls];
+        _player = [[KPViewController alloc] initWithConfiguration:config];
+        NSLog(@"Player configured");
+        
+    }
+    return _player;
+}
+
+- (void)hideHTMLControls {
+    // chromeless config
+    // Set AutoPlay as configuration on player (same like setting a flashvar)
+    [config addConfigKey:@"controlBarContainer.plugin" withValue:@"false"];
+    [config addConfigKey:@"topBarContainer.plugin" withValue:@"false"];
+    [config addConfigKey:@"largePlayBtn.plugin" withValue:@"false"];
+    [config addConfigKey:@"loadingSpinner.plugin" withValue:@"false"];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    [self presentViewController:self.player animated:YES completion:nil];
 }
 
 @end
