965f34acb20732a305289443f73e7f44e962b236
Jonathan <jonigl@gmail.com>
2016-03-21 17:52:26 -0300

    Initial Commit

---
+++ kaltura-playlist-ios/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  kaltura-playlist-ios
+//
+//  Created by Jonathan Lowenstern on 3/21/16.
+//  Copyright © 2016 Jonathan Lowenstern. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ kaltura-playlist-ios/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  kaltura-playlist-ios
+//
+//  Created by Jonathan Lowenstern on 3/21/16.
+//  Copyright © 2016 Jonathan Lowenstern. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
